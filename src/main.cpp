5297ec1670eb8709fbfdcc077f6a4b32c971acdf
Florian Mauracher <florian.mauracher@gmx.net>
2017-12-12 09:17:31 +0100

    Basic I2C and SPI sensor test

--- src/main.cpp
+++ src/main.cpp
@@ -18,14 +18,33 @@ void led_thread() {
     }
 }
 
+char tx_buffer[8] = {0};
+char rx_buffer[8] = {0};
+const int bno055_addr = 0x28 << 1;
+
 int main() {
     printf("\r\nRESET\r\n");
     thread.start(led_thread);
+    cs_bme680 = 1;
+    cs_adc = 1;
 
     while(1) {
         logger.printf("LOG\r\n");
         cdh.printf("CDH\r\n");
 
+        tx_buffer[0] = 0x0;
+        bno.write(bno055_addr, &tx_buffer[0], 1);
+        bno.read(bno055_addr, &rx_buffer[0], 1);
+        printf("BNO055 CHIP_ID: 0x%02x\r\n", rx_buffer[0]);
+        memset(rx_buffer, 0, sizeof(rx_buffer));
+
+        cs_bme680 = 0;
+        tx_buffer[0] = 0x50 | 0x80;
+        spi.write(&tx_buffer[0], 1, &rx_buffer[0], 2);
+        printf("BME680 CHIP_ID: 0x%02x\r\n", rx_buffer[1]);
+        cs_bme680 = 1;
+
+        memset(rx_buffer, 0, sizeof(rx_buffer));
         Thread::wait(1000);
     }
 }
