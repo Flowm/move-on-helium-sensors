80aa791c17ab3673216773e1679567084e7a8f29
Florian Mauracher <florian.mauracher@gmx.net>
2018-01-24 21:11:12 +0100

    Only start sensor thread if sensor setup is successful

--- src/modules/IMU/BNO055.cpp
+++ src/modules/IMU/BNO055.cpp
@@ -1,20 +1,21 @@
 #include "BNO055.hpp"
 
-void BNO055::setup() {
-    getChipId();
+bool BNO055::setup() {
+    return getChipId();
 }
 
 void BNO055::update() {
 }
 
-void BNO055::getChipId() {
+bool BNO055::getChipId() {
+    memset(rx_buffer, 0, sizeof(rx_buffer));
     i2c.write(i2c_addr, &tx_buffer[0], 1);
     i2c.read(i2c_addr, &rx_buffer[0], 1);
     if (rx_buffer[0] == chip_id) {
         printf("BNO055 CHIP_ID: 0x%02x valid\r\n", rx_buffer[0]);
+        return true;
     } else {
         printf("BNO055 CHIP_ID: 0x%02x invalid\r\n", rx_buffer[0]);
+        return false;
     }
-
-    memset(rx_buffer, 0, sizeof(rx_buffer));
 }
