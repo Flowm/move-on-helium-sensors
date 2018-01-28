5255a53526df5d3af10201cef0c9c1ccbb492e66
Alexander Lill <alexander.lill@tum.de>
2018-01-28 23:41:31 +0100

    Add new data to the output of the Sensors class

--- src/modules/Sensors/Sensors.cpp
+++ src/modules/Sensors/Sensors.cpp
@@ -16,11 +16,22 @@ void Sensors::loop() {
 void Sensors::log() {
     storage.lock();
     SensorData* data = storage.data;
-    logger.printf("D=ENV,T=%4.2f,H=%4.2f;P=%6.2f;G=%6.3f\r\n",
+    logger.printf("D=ENV,T=%4.2f,H=%4.2f;P=%6.2f;G=%6.3f "
+                  "ACCEL x=%4.2f, y=%4.2f, z=%4.2f "
+                  "MAG x=%4.2f, y=%4.2f, z=%4.2f "
+                  "GYRO x=%4.2f, y=%4.2f, z=%4.2f "
+                  "QUAT w=%4.2f, x=%4.2f, y=%4.2f, z=%4.2f "
+                  "ANGLES h=%4.2f, r=%4.2f, p=%4.2f "
+                  "\r\n",
                   data->env.temperature,
                   data->env.humidity,
                   data->env.pressure,
-                  data->env.gasresistance
+                  data->env.gasresistance,
+                  data->imu.accel.x, data->imu.accel.y, data->imu.accel.z,
+                  data->imu.mag.x, data->imu.mag.y, data->imu.mag.z,
+                  data->imu.gyro.x, data->imu.gyro.y, data->imu.gyro.z,
+                  data->imu.quaternion.r, data->imu.quaternion.i, data->imu.quaternion.j, data->imu.quaternion.k,
+                  data->imu.orientation.h, data->imu.orientation.r, data->imu.orientation.p
                  );
     storage.unlock();
 }
