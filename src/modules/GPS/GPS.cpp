5c6f24af58e165bb5289b02859afe3de1b39afb9
Tejas Kale <tejas.kale@tum.de>
2018-01-29 20:24:43 +0100

    Fix indentation

--- src/modules/GPS/GPS.cpp
+++ src/modules/GPS/GPS.cpp
@@ -8,26 +8,26 @@
 #include <GPS/GPS.hpp>
 
 void GPS::setup(){
-//	i2c.frequency(100000)
-//	i2c.read()
-//	char data[100];
-//	readFromAddress(0xFD,)
-	char data[100];
-	int ack = i2c.read(M8_ADDRESS,data, false);
-	printf("GPS ACK: %d\r\n",ack);
+//  i2c.frequency(100000)
+//  i2c.read()
+//  char data[100];
+//  readFromAddress(0xFD,)
+    char data[100];
+    int ack = i2c.read(M8_ADDRESS,data, false);
+    printf("GPS ACK: %d\r\n",ack);
 
-	readFromRegister(M8_ADDRESS, M8_DATALEN, data, 2, false);
+    readFromRegister(M8_ADDRESS, M8_DATALEN, data, 2, false);
 
 }
 
 void GPS::readFromRegister(uint8_t address, uint8_t reg, char* data, uint8_t len, bool repeated){
 
-	int ack = 0;
-	ack = i2c.write(address);
-	printf("ADDRESS ACK: %d\r\n",ack);
-	ack = i2c.write(reg);
-	printf("REG ADDRESS ACK: %d\r\n",ack);
-	ack = i2c.read(address, data, len, repeated);
-	printf("%d : %d",data[0],data[1]);
+    int ack = 0;
+    ack = i2c.write(address);
+    printf("ADDRESS ACK: %d\r\n",ack);
+    ack = i2c.write(reg);
+    printf("REG ADDRESS ACK: %d\r\n",ack);
+    ack = i2c.read(address, data, len, repeated);
+    printf("%d : %d",data[0],data[1]);
 }
 
--- src/modules/GPS/GPS.hpp
+++ src/modules/GPS/GPS.hpp
@@ -9,20 +9,20 @@
 
 #include<mbed.h>
 
-#define M8_ADDRESS	0x42
-#define M8_DATALEN 	0xFD
-#define M8_DATA		0xFF
+#define M8_ADDRESS  0x42
+#define M8_DATALEN  0xFD
+#define M8_DATA     0xFF
 
 
 class GPS {
 public:
-	GPS(I2C &i2c):
-		i2c(i2c)
-		{setup();};
+    GPS(I2C &i2c):
+        i2c(i2c)
+        {setup();};
 private:
 
-	void readFromRegister(uint8_t address, uint8_t reg, char* data, uint8_t len, bool repeated);
-	void setup();
-	I2C& i2c;
+    void readFromRegister(uint8_t address, uint8_t reg, char* data, uint8_t len, bool repeated);
+    void setup();
+    I2C& i2c;
 };
 
