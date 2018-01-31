8956b8f7b61abd5671777379bcfa05114c19fe6a
Tejas Kale <tejas.kale@tum.de>
2018-01-31 23:40:50 +0100

    Influxdb line format for direct import.

--- src/modules/Sensors/Sensors.cpp
+++ src/modules/Sensors/Sensors.cpp
@@ -19,32 +19,34 @@ void Sensors::loop() {
 void Sensors::log() {
     storage.lock();
     SensorData* data = storage.data;
-    logger.printf("D=ENV,"
-                  "T=%4.2f,H=%4.2f,P=%6.2f,G=%6.3f"
-                  "\r\n"
-                  "D=IMU,"
-                  "ACC_X=%4.2f,ACC_Y=%4.2f,ACC_Z=%4.2f,"
-                  "MAG_X=%4.2f,MAG_Y=%4.2f,MAG_Z=%4.2f,"
-                  "GYRO_X=%4.2f,GYRO_Y=%4.2f,GYRO_Z=%4.2f,"
-                  "QUAT_W=%4.2f,QUAT_X=%4.2f,QUAT_Y=%4.2f,QUAT_Z=%4.2f,"
-                  "ANG_X=%4.2f,ANG_Y=%4.2f,ANG_Z=%4.2f,"
-                  "TEMP_ACC=%d,TEMP_GYRO=%d"
+    logger.printf("ENV "
+                  "T=%.4f,H=%.4f,P=%.4f,G=%.4f"
                   "\r\n",
                   data->env.temperature,
                   data->env.humidity,
                   data->env.pressure,
-                  data->env.gasresistance,
+                  data->env.gasresistance);
+    logger.printf("IMU "
+                  "ACC_X=%.4f,ACC_Y=%.4f,ACC_Z=%.4f,"
+                  "MAG_X=%.4f,MAG_Y=%.4f,MAG_Z=%.4f,"
+                  "GYRO_X=%.4f,GYRO_Y=%.4f,GYRO_Z=%.4f,"
+                  "QUAT_W=%.4f,QUAT_X=%.4f,QUAT_Y=%.4f,QUAT_Z=%.4f,"
+                  "ANG_X=%.4f,ANG_Y=%.4f,ANG_Z=%.4f,"
+                  "TEMP_ACC=%d,TEMP_GYRO=%d"
+                  "\r\n",
                   data->imu.accel.x, data->imu.accel.y, data->imu.accel.z,
                   data->imu.mag.x, data->imu.mag.y, data->imu.mag.z,
                   data->imu.gyro.x, data->imu.gyro.y, data->imu.gyro.z,
                   data->imu.quaternion.w, data->imu.quaternion.x, data->imu.quaternion.y, data->imu.quaternion.z,
                   data->imu.orientation.x, data->imu.orientation.y, data->imu.orientation.z,
-                  data->imu.temp_accel, data->imu.temp_gyro
-                 );
-    logger.printf("D=TMP");
-    for(int i = 0; i < temperature.getNumDevices(); i++) {
-        logger.printf(",T%d=%2.4f", i, data->temp[i].temp);
+                  data->imu.temp_accel, data->imu.temp_gyro);
+    if(temperature.getNumDevices() > 0) {
+        logger.printf("TMP ");
+        logger.printf("T%d=%.4f", 0, data->temp[0].temp);
+        for(int i = 1; i < temperature.getNumDevices(); i++) {
+            logger.printf(",T%d=%.4f", i, data->temp[i].temp);
+        }
+        logger.printf("\r\n");
+        storage.unlock();
     }
-    logger.printf("\r\n");
-    storage.unlock();
 }
