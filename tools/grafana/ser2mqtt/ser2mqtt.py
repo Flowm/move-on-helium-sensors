8f006ac62621328b45a855902c89285b43bfb52f
Florian Mauracher <florian.mauracher@gmx.net>
2018-02-01 20:36:41 +0100

    Wrap pyserial for serial port access

--- tools/grafana/ser2mqtt/ser2mqtt.py
+++ tools/grafana/ser2mqtt/ser2mqtt.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import logging
 import sys
 import paho.mqtt.client as mqtt
+from serial_wrap import SerialWrap
 
 MQTT_IP = "localhost"
 MQTT_PORT = 1883
@@ -11,15 +12,21 @@ logging.basicConfig(level=logging.DEBUG, format="(%(threadName)-10s) %(levelname
 logging.info("ser2mqtt starting")
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    logging.info("MQTT connected with result code " + str(rc))
 
 client = mqtt.Client(client_id="ser2mqtt")
 client.on_connect = on_connect
 client.connect(MQTT_IP, MQTT_PORT, 60)
 client.loop_start()
 
-while True:
-    line = sys.stdin.readline().strip()
+serial_console = SerialWrap("/dev/ttyACM0", 9600, timeout=0.2)
+
+def handle_input():
+    line = serial_console.try_readline_decode()
+    if not line:
+        return
+    logging.debug("RECV: " + line)
+
     client.publish("sensors-raw", line)
 
     try:
@@ -28,10 +35,14 @@ while True:
             try:
                 name, value = entry.split('=')
             except ValueError:
-                continue
+                return
             else:
                 client.publish("sensors/" + type + "/" + name, value)
-                print("Publish " + "sensors/" + type + "/" + name + " Value: " +  value)
+                logging.info("Publish " + "sensors/" + type + "/" + name + " Value: " +  value)
     except ValueError:
-        print('Invalid data in line: ' + line)
-        continue
+        logging.info('Invalid data in line: ' + line)
+        return
+
+
+while True:
+    handle_input()
