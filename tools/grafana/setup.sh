600e63f9b35eca89d8cccfe7d5d24122910d5819
Florian Mauracher <florian.mauracher@gmx.net>
2018-02-11 02:14:52 +0100

    Create retention policy for influxdb

--- tools/grafana/setup.sh
+++ tools/grafana/setup.sh
@@ -6,4 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/config/etc/telegraf/telegraf.d/*.conf /etc/telegraf/telegraf.d/
 systemctl restart telegraf
 
+influx -execute 'CREATE RETENTION POLICY "moveon" ON telegraf DURATION 14d REPLICATION 1 default'
+
 cd $DIR/ser2mqtt && make install
