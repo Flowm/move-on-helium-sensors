ad25db7d3cf76b5cd2fe3e4308111619d63403d5
Fabian Schöttl <fabian.schoettl@tum.de>
2018-02-23 00:23:46 +0100

    Fixed ser2mqtt_cdh.py

--- tools/grafana/daemon/ser2mqtt_cdh/ser2mqtt_cdh.py
+++ tools/grafana/daemon/ser2mqtt_cdh/ser2mqtt_cdh.py
@@ -37,21 +37,21 @@ def connectSerial():
 subsystemNames = ["SENS", "EPS1", "EPS2", "CAM", "PAYL"]
 subsystemResultNames = ["CF", "TR", "TH", "IS", "IN", "TD", "TF", "IE", "IC", "OK"]
 subsystemResultOrder = [9, 8, 1, 2, 5, 6, 3, 4, 7, 0]
-comResultNames = ["OK", "NR"]
+comResultNames = ["OK", "NR"] 
 
 N = len(subsystemNames)
 NR = len(subsystemResultNames)
 NC = len(comResultNames)
 
 structFormat = "".join([
-    "L", # time
-    "H", # cpu time
-    "B", # sdUSed
-    "B", # status
-    "%iH" % N, # bytes
-    "%iB" % (N * NR), # subsystem
-    "%iB" % (N * NC) #com
-])
+            "L", # time
+            "H", # cpu time
+            "B", # sdUSed
+            "B", # status
+            "%iH" % N, # bytes
+            "%iB" % (N * NR), # subsystem
+            "%iB" % (N * NC) #com
+            ])
 
 class Stats:
     def __init__(self, bytes):
@@ -63,37 +63,37 @@ class Stats:
         self.bytes = [0 for i in range(0, N)]
         self.subsystemResults = [[0 for j in range(0, NR)] for i in range(0, N)]
         self.comResults = [[0 for j in range(0, NC)] for i in range(0, N)]
-
+        
         for i in range(0, N):
             self.bytes[i] = fields.pop(0)
-            for i in range(0, N):
-                for j in range(0, NR):
-                    self.subsystemResults[i][j] = fields.pop(0)
-                    for i in range(0, N):
-                        for j in range(0, NC):
-                            self.comResults[i][j] = fields.pop(0)
-
+        for i in range(0, N):
+            for j in range(0, NR):
+                self.subsystemResults[i][j] = fields.pop(0)
+        for i in range(0, N):
+            for j in range(0, NC):
+                self.comResults[i][j] = fields.pop(0)           
+                
 def formatTable(data, rowNames, columnNames, title):
     n = len(data)
     m = len(data[0])
-
+    
     result = ""
     result += (title + " ").ljust(8 + m * 4, "=") + "\n"
     result += " " * 8
     for j in range(0, m):
         result += "%-3s " % columnNames[j]
+    result += "\n"
+    for i in range(0, n):
+        result += "%4s:   " % rowNames[i]
+        for j in range(0, m):
+            v = data[i][j]
+            if v > 0:
+                result += "%-3i " % v
+            else:
+                result += ".   "
         result += "\n"
-        for i in range(0, n):
-            result += "%4s:   " % rowNames[i]
-            for j in range(0, m):
-                v = data[i][j]
-                if v > 0:
-                    result += "%-3i " % v
-                else:
-                    result += ".   "
-                    result += "\n"
-                    return result
-
+    return result
+                
 def formatStats(stats):
     result = "\n"
     result += "STATS ".ljust(28, "=") + "\n"
@@ -102,36 +102,40 @@ def formatStats(stats):
     result += "used memory:  %02.2f %%\n" % (stats.sdUsed * 100)
     result += "status:       0x%.2X\n" % stats.status
     result += "\n"
-
+    
     result += formatTable(stats.subsystemResults, subsystemNames, subsystemResultNames, "SUBSYSTEM RESULTS")
     result += "\n"
-
+    
     result += "RECEIVED ".ljust(12, "=") + "\n"
     for i in range(0, N):
         result += "%4s:   " % subsystemNames[i]
         result += "%i" % stats.bytes[i]
         result += "\n"
-        result += "\n"
-
+    result += "\n"
+    
     result += formatTable(stats.comResults, subsystemNames, comResultNames, "COM RESULTS")
-
+    
     return result[:-1]
-
+        
 def handle_input():
-    bytes = console.readline().strip()
-
+    bytes = console.readline()
+    
     try:
         if len(bytes) == 0:
             return
-        if bytes[0] == 0x04:
+        bytes = bytes.strip()
+        if len(bytes) == 0:
+            client.publish("CDH", " ")
+        
+        if len(bytes) > 0 and bytes[0] == 0x04:
             bytes = bytes[1:]
             if len(bytes) == 78:
                 stats = Stats(bytes)
                 output = formatStats(stats)
                 client.publish("CDH", output)
-            else:
-                line = bytes.decode()
-                client.publish("CDH", line)
+        else:
+            line = bytes.decode()
+            client.publish("CDH", line)
     except UnicodeDecodeError:
         logging.error(traceback.format_exc())
         logging.debug(bytes)
@@ -139,10 +143,10 @@ def handle_input():
 while True:
     if client == None:
         startMQTT()
-
+        
     if console == None:
         connectSerial()
-
+        
     while True:
         try:
             handle_input()
