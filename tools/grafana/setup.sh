0e7d1b85a21f57d5eb33d1412c14b3e60bee0d46
Florian Mauracher <florian.mauracher@gmx.net>
2018-02-22 23:55:49 +0100

    Restart ser2mqtt services after deploy

--- tools/grafana/setup.sh
+++ tools/grafana/setup.sh
@@ -14,13 +14,4 @@ influx -execute 'CREATE RETENTION POLICY "moveon" ON telegraf DURATION 14d REPLI
 cd $DIR/bin/mqttserial && make install
 
 # Daemon
-cd $DIR/daemon
-for srv in *; do
-    if ![ -f $srv/$srv.service]; then
-        continue
-    fi
-    cp $srv/$srv.service /etc/systemd/system/
-    systemctl daemon-reload
-    systemctl enable $srv
-    systemctl restart $srv
-done
+$DIR/daemon/setup.sh
