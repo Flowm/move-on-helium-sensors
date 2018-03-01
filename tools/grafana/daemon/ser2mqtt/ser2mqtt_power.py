247fc2b81f80442f96dcfb296adf8666f06c46a2
Fabian Schöttl <fabian.schoettl@tum.de>
2018-03-01 16:45:29 +0100

    Changed power mesaurement to differential measurement

--- tools/grafana/daemon/ser2mqtt/ser2mqtt_power.py
+++ tools/grafana/daemon/ser2mqtt/ser2mqtt_power.py
@@ -9,16 +9,16 @@ adc = Adafruit_ADS1x15.ADS1115()
 mqtt = MqttBroker()
 
 GAIN = 1
+diffs = [0, 3]
 
-print('Reading ADS1x15 values, press Ctrl-C to quit...')
-print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
-print('-' * 37)
 while True:
-    values = [0]*4
-    for i in range(4):
-        values[i] = adc.read_adc(i, gain=GAIN) / (2**15) * 4.096
-        
-    for i in range(4):
+    values = [0] * 2
+
+    for i in range(2):
+        values[i] = adc.read_adc_difference(diffs[i], gain=GAIN)
+        values[i] = values[i] * 4.096 / (2**15)
+    
+    for i in range(2):
         mqtt.publish("power/voltage_%i" % i, str(values[i]))
     
     time.sleep(0.5)
