266da14557f39d1404f32c67351e18dccda6ce64
Florian Mauracher <florian.mauracher@gmx.net>
2018-03-15 04:18:55 +0100

    Move sensor ascii parsing to separate file

--- tools/grafana/daemon/ser2mqtt/ser2mqtt_sen.py
+++ tools/grafana/daemon/ser2mqtt/ser2mqtt_sen.py
@@ -1,61 +1,29 @@
 #!/usr/bin/env python3
 
 import logging
-import json
 from lib.input.serial_wrap import SerialWrap
+from lib.parse.sen_ascii_parse import SENParse
 from lib.output.mqtt_broker import MqttBroker
 
 logging.basicConfig(level=logging.DEBUG,
                     format="(%(threadName)-10s) %(levelname)s %(message)s")
-logging.info("ser2mqtt starting")
-
 mqtt = MqttBroker()
-serial_console = SerialWrap("/dev/ttyACM0", 115200, timeout=0.2)
-
-
-def str2digit(value):
-    if value == "nan":
-        return None
-    try:
-        return int(value)
-    except ValueError:
-        try:
-            return float(value)
-        except ValueError:
-            return None
-
-
-def handle_input():
-    line = serial_console.try_readline_decode()
-    if not line:
-        return
-    logging.debug("RECV: " + line)
-
-    mqtt.publish("sensors-raw", line)
-
-    try:
-        data_dict = {}
-        type, data = line.split(' ')
-        for entry in data.split(','):
-
-            try:
-                name, value = entry.split('=')
-            except ValueError:
-                return
-            else:
-                data_dict[name] = str2digit(value)
-                mqtt.publish("sensors/%s/%s" % (type, name), value)
-                logging.info("Pub sensors/%s/%s: %s" % (type, name, value))
-        if type == "GPS":
-            mqtt.publish("sensors-processed/%s" % type, json.dumps(data_dict))
-    except ValueError:
-        logging.info('Invalid data in line: ' + line)
-        return
+serial = SerialWrap("/dev/ttyACM0", 115200, timeout=0.2)
+parser = SENParse()
 
 
 def main():
+    logging.info("ser2mqtt_sen starting")
+
     while True:
-        handle_input()
+        line = serial.try_readline_decode()
+        if not line:
+            continue
+        logging.debug("RECV: " + line)
+
+        for (key, value) in parser.parse_packet(line):
+                logging.info("Pub %s: %s" % (key, value))
+                mqtt.publish(key, value)
 
 
 if __name__ == '__main__':
