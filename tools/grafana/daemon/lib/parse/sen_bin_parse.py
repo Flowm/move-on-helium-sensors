fd424cecb66c0e73b89529528d9a3e571bde7f90
Florian Mauracher <florian.mauracher@gmx.net>
2018-04-04 10:47:02 +0200

    Accept sen packets without header

--- tools/grafana/daemon/lib/parse/sen_bin_parse.py
+++ tools/grafana/daemon/lib/parse/sen_bin_parse.py
@@ -7,15 +7,20 @@ class SenBinParse:
     def __init__(self):
         self.ascii_parser = SenAsciiParse()
 
-    def parse_packet(self, packet):
+    def parse_packet(self, packet, with_header=True):
         if len(packet) < 10:
             return
 
-        length, status = packet[:2]
-        data = packet[2:-1]
-        invalid_chunks = packet[-1]
-        #logging.debug("BIN IN: CHK=%d DATA%s" % (invalid_chunks, packet))
-        logging.debug("BIN IN: CHK=%d" % invalid_chunks)
+        length = len(packet)
+        status = 0
+        data = packet
+        invalid_chunks = 0
+
+        if with_header:
+            length, status = packet[:2]
+            data = packet[2:-1]
+            invalid_chunks = packet[-1]
+            logging.debug("BIN IN: CHK=%d" % invalid_chunks)
 
         #if invalid_chunks != 0:
         #    return
